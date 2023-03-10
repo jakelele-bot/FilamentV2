ea293679dc33a7cf780ea389189a3aa9fc3d42af
jakob <jakobspars@gmail.com>
2023-03-10 14:13:42 +0100

    Update ARDUINO.ino

--- ARDUINO/ARDUINO/ARDUINO.ino
+++ ARDUINO/ARDUINO/ARDUINO.ino
@@ -1,6 +1,7 @@
 #include "libs/Encoder/Encoder.h"
   //der er mange fejl boel men jeg (JAKOB) har fået lavet så at vi har biblotekerne inde i en mappe "libs"
   //man kalder biblotekerne ved at gøre således "libs/Encoder/Encoder.h"
+  //DET SKAL VÆRE EN KLASSE BOEL!! JEG TROR IKKE DU KAN BRUGE SÅ MEGET AF DET HER...... Pls lav noget nyt og smart:)
 void setup() {
   Serial.begin(9600);
   pinMode(SW, INPUT_PULLUP);
