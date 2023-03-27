afaf882f8cfd91a1d71aa6404af6f4effc8fe5fc
b0el <114650269+b0el@users.noreply.github.com>
2023-03-27 09:59:02 +0200

    pushing p

--- ARDUINO/ARDUINO/ARDUINO.ino
+++ ARDUINO/ARDUINO/ARDUINO.ino
@@ -4,6 +4,7 @@
 //#include <PID_v1.h> //af Brett Beauregard bliver brugt i "regPID.h"
 #include "LCD.h" //header fil til LCD display menuen
 #include "thermo.h"
+#include "setupOpstart.h"
 
 
 //Vi reservere pin fra 42 til 53 digital på arduino mega, til stepper drivers(TB6600)
@@ -14,37 +15,7 @@ thermo thermo1(1, 2, 3);
 //for at læse data skal man bruge strukturen nedenfor
 //Serial.println(thermo1.readCelsius());
 void setup() {
-  
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  scale.set_scale(567.916);  // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();               // reset the scale to 0
-
-  pinMode(PB2, INPUT_PULLUP); //RotaryCLK
-  pinMode(PB4, INPUT_PULLUP); //RotaryDT
-  pinMode(PB3, INPUT_PULLUP); //Button
-
-  //------------------------------------------------------
-  lcd.init();                      // initialize the lcd   
-  lcd.backlight();
-  //------------------------------------------------------
-  lcd.setCursor(0,0); //Defining positon to write from first row, first column .
-  lcd.print("Menu for");
-  lcd.setCursor(0,1); //Second row, first column
-  lcd.print("Filament Extruder"); 
-  lcd.setCursor(0,2); //Second row, first column
-  lcd.print("Version: v1.1"); 
-  delay(5000); //wait 2 sec
-  
-  lcd.clear(); //clear the whole LCD
-  
-  printLCD(); //print the stationary parts on the screen
-  //------------------------------------------------------
-  //Store states of the rotary encoder
-  CLKPrevious = digitalRead(RotaryCLK);
-  DTPrevious = digitalRead(RotaryDT);
-      
-  attachInterrupt(digitalPinToInterrupt(RotaryCLK), rotate, CHANGE); //CLK pin is an interrupt pin
-  attachInterrupt(digitalPinToInterrupt(PushButton), pushButton, FALLING); //PushButton pin is an interrupt pin
+  setupStart();
 }
 
 void loop() {
@@ -73,8 +44,8 @@ void loop() {
     if (menu4_Value == 1){
     ProgramStart(); 
   }
-}
 
+}
 
 void ProgramStart(){
   if (stateRefresh == 1){
